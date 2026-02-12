eb8e8c2b16c4e53eca066ba59f0db5b8b678db6b
Alexey Trukhlyayev <exact84@gmail.com>
2026-02-13 01:10:25 +0500

    feat: add router and layout

--- smart-home-ui/src/app/app.routes.ts
+++ smart-home-ui/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () => import('./components/login/login').then((m) => m.Login),
+  },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () => import('./components/dashboard/dashboard').then((m) => m.Dashboard),
+      },
+    ],
+  },
+  // {
+  // path: '**',
+  // loadComponent: () => import('./components/404/404').then((m) => m.404),
+  // },
+];
--- smart-home-ui/src/app/app.ts
+++ smart-home-ui/src/app/app.ts
@@ -1,31 +1,12 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Dashboard } from './components/dashboard/dashboard';
-import { Sidebar } from './components/sidebar/sidebar';
-import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashboard, Sidebar, MatIconModule],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('Smart-home-ui');
-  sidebarOpen = signal(false);
-  windowWidth = signal(window.innerWidth);
-
-  constructor() {
-    window.addEventListener('resize', () => {
-      this.windowWidth.set(window.innerWidth);
-    });
-  }
-
-  showSidebar = computed(() => {
-    return this.windowWidth() >= 768 || this.sidebarOpen();
-  });
-
-  toggleSidebar() {
-    this.sidebarOpen.update((open) => !open);
-  }
 }
